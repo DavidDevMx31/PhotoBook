fcffed4fe612149e05bb1f7050f7c73a63fb1613
David Castillo <davidc.mendoza31@gmail.com>
2020-08-29 15:28:16 -0500

    Define interfaz de los interactors

---
+++ AlbumsWebApp.Domain/Implementations/CommentInteractor.cs
@@ -0,0 +1,17 @@
+﻿using AlbumsWebApp.Domain.DomainInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumsWebApp.Domain.Implementations
+{
+    public class CommentInteractor : ICommentInteractor
+    {
+        public void GetCommentsForPhoto(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlbumsWebApp.Domain/Implementations/PhotoInteractor.cs
@@ -0,0 +1,17 @@
+﻿using AlbumsWebApp.Domain.DomainInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumsWebApp.Domain.Implementations
+{
+    public class PhotoInteractor : IPhotoInteractor
+    {
+        public void GetPhotosForAlbum(int albumId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
