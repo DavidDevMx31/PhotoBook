ffe991e794c3b29e163ea3eab5959b5dd95a4f9b
David Castillo <davidc.mendoza31@gmail.com>
2020-08-29 17:10:12 -0500

    Correccion de typos

--- AlbumsWebAppMVC/Scripts/javascript/albums.js
+++ AlbumsWebAppMVC/Scripts/javascript/albums.js
@@ -1,13 +1,13 @@
 ﻿function showAlbumPhotos() {
-    console.log(document.getElementById("albums").value);
+    alert("Elegiste el álbum " + document.getElementById("albums").value);
     var albumId = document.getElementById("albums").value;
-    $.ajax({
-        url: '/Albums/GetAlbumPhotos',
-        type: 'GET',
-        dataType: 'JSON',
-        data: { albumId = albumId },
-        success: function (response) {
-            console.log(JSON.stringify(response));
-        }
-    });
+    //$.ajax({
+    //    url: '/Albums/GetAlbumPhotos',
+    //    type: 'GET',
+    //    dataType: 'JSON',
+    //    data: { albumId = albumId },
+    //    success: function (response) {
+    //        console.log(JSON.stringify(response));
+    //    }
+    //});
 }
