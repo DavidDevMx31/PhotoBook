715d77510a770f135cab6e47b4fe9219aacf0280
David Castillo <davidc.mendoza31@gmail.com>
2020-11-06 16:17:07 -0600

    Agrega funcionalidad de carrousel en Fotos

---
+++ AlbumsWebAppMVC/Scripts/javascript/photos.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $('.thumbnail').click(function () {
+        var selectedImageKey = $(this).find('img').attr('data-image-key');
+        $('.carousel-inner div').removeClass("active");
+        $('#' + selectedImageKey).addClass('active');
+        $('#photosModal').modal('show');
+    });
+
+});
